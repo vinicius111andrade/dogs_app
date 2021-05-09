a6aa26aa4d9284410b46b02e982807eb63ec32d6
Vinicius de Melo Andrade <vinicius111andrade@gmail.com>
2021-05-09 17:02:20 -0300

    views e viewmodels funcionais, model com hardocoded data

--- Dogs/app/src/main/java/com/vdemelo/dogs/view/DetailFragment.kt
+++ Dogs/app/src/main/java/com/vdemelo/dogs/view/DetailFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.vdemelo.dogs.R
+import com.vdemelo.dogs.viewmodel.DetailViewModel
+import com.vdemelo.dogs.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
+    private lateinit var viewModel: DetailViewModel
     private var dogUuid = 0
 
     override fun onCreateView(
@@ -24,8 +29,25 @@ class DetailFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        //preciso receber qual dog o usuario clicou, n sei fazer isso
         arguments?.let {
             dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
         }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.dogLiveData.observe(this, Observer {dog ->
+            dog?.let {
+                dogName.text = dog.dogBreed
+                dogPurpose.text = dog.bredFor
+                dogTemperament.text = dog.temperament
+                dogLifespan.text = dog.lifeSpan
+            }
+        })
     }
 }
--- Dogs/app/src/main/java/com/vdemelo/dogs/viewmodel/DetailViewModel.kt
+++ Dogs/app/src/main/java/com/vdemelo/dogs/viewmodel/DetailViewModel.kt
@@ -1,4 +1,16 @@
 package com.vdemelo.dogs.viewmodel
 
-class DetailViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.vdemelo.dogs.model.DogBreed
+
+class DetailViewModel : ViewModel() {
+
+    val dogLiveData = MutableLiveData<DogBreed>()
+
+    fun fetch() {
+        val dog = DogBreed("1", "Corgi", "15 years", "breedGroup", "bredFor", "temp", "")
+        dogLiveData.value = dog
+    }
 }
+
