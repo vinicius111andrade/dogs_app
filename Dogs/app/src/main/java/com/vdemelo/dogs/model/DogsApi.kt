efc82e2147848dd1e0a539583f0f0a14440a23fb
Vinicius de Melo Andrade <vinicius111andrade@gmail.com>
2021-07-11 18:39:39 -0300

    Limpeza.

--- Dogs/app/src/main/java/com/vdemelo/dogs/model/DogsApi.kt
+++ Dogs/app/src/main/java/com/vdemelo/dogs/model/DogsApi.kt
@@ -3,10 +3,6 @@ package com.vdemelo.dogs.model
 import io.reactivex.Single
 import retrofit2.http.GET
 
-/**
- * Created on 5/27/2021
- * @author Vinicius de Melo Andrade
- */
 interface DogsApi {
     @GET("DevTides/DogsApi/master/dogs.json")
     fun getDogs(): Single<List<DogBreed>>
