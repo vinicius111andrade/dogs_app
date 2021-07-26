5700b5288305fab395eac49f14ce2da77d090372
Vinícius de Melo Andrade <vinicius111andrade@gmail.com>
2021-07-26 17:24:41 -0300

    Companion object que vai instanciar o database e previnir que exista mais de uma instância desse objeto.

---
+++ Dogs/app/src/main/java/com/vdemelo/dogs/model/DogDataBase.kt
@@ -0,0 +1,28 @@
+package com.vdemelo.dogs.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [DogBreed::class], version = 1)
+abstract class DogDataBase: RoomDatabase() {
+    abstract fun dogDao(): DogDao
+
+    companion object {
+        @Volatile private var instance: DogDataBase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDataBase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDataBase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            DogDataBase::class.java,
+            "dogdatabase"
+        ).build()
+    }
+}
